24495cf48a9d8bdee98cf1b9fc08e0691cf295aa
unknown <ajfoucaultmo@mail.usf.edu>
2022-05-24 22:52:11 -0700

    Update portfolio of projects

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,43 +1,42 @@
-import React from "react";
-import GoogleClone from "../assets/google-clone.jpg";
-import IMDBClone from "../assets/imdb-clone.jpg";
-import InstagramClone from "../assets/instagram-clone.jpg";
-import Forkify from "../assets/forkify.jpg";
-import Mapty from "../assets/mapty.jpg";
-import BluePidgeon from "../assets/bluepidgeon.jpg";
-import SupportDesk from "../assets/supportdesk.jpg";
+import React from 'react';
+import GoogleClone from '../assets/google-clone.jpg';
+import IMDBClone from '../assets/imdb-clone.jpg';
+import InstagramClone from '../assets/instagram-clone.jpg';
+import Forkify from '../assets/forkify.jpg';
+import CrwnClothing from '../assets/crwn-clothing.jpg';
+import SupportDesk from '../assets/supportdesk.jpg';
 
 const Work = () => {
   return (
-    <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
-      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
-        <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
             Work
           </p>
-          <p className="py-6">// Please check out some of my recent work:</p>
+          <p className='py-6'>// Please check out some of my recent work:</p>
         </div>
 
         {/* Container */}
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
           {/* Grid Item */}
           <div
             style={{ backgroundImage: `url(${InstagramClone})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Instagram Clone
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                Instagram Clone Social Media App
               </span>
-              <div className="pt-8 text-center">
-                <a href="https://instagram-clone.vercel.app//">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+              <div className='pt-8 text-center'>
+                <a href='https://instagram-clone.vercel.app//'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a>
-                <a href="https://github.com/ajfm88/instagram-clone/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                <a href='https://github.com/ajfm88/instagram-clone/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
                 </a>
@@ -45,22 +44,22 @@ const Work = () => {
             </div>
           </div>
           <div
-            style={{ backgroundImage: `url(${Forkify})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            style={{ backgroundImage: `url(${CrwnClothing})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Forkify Recipe App
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                Crwn Clothing eCommerce App
               </span>
-              <div className="pt-8 text-center">
-                <a href="https://forkify-v2.netlify.app/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+              <div className='pt-8 text-center'>
+                <a href='https://crwn-clothing-ajfm88.netlify.app/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a>
-                <a href="https://github.com/ajfm88/CompleteJavaScriptCourseJonas/tree/main/18-forkify/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                <a href='https://github.com/ajfm88/crwn-clothing/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
                 </a>
@@ -70,21 +69,21 @@ const Work = () => {
           {/* Grid Item */}
           <div
             style={{ backgroundImage: `url(${GoogleClone})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Google Clone
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                Google Clone Search Engine App
               </span>
-              <div className="pt-8 text-center">
-                <a href="https://google-clone-ajfm88.vercel.app//">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+              <div className='pt-8 text-center'>
+                <a href='https://google-clone-ajfm88.vercel.app//'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a>
-                <a href="https://github.com/ajfm88/google-clone/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                <a href='https://github.com/ajfm88/google-clone/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
                 </a>
@@ -92,22 +91,22 @@ const Work = () => {
             </div>
           </div>
           <div
-            style={{ backgroundImage: `url(${Mapty})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            style={{ backgroundImage: `url(${Forkify})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Mapty Workout App
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                Forkify Recipe App
               </span>
-              <div className="pt-8 text-center">
-                <a href="https://mapty.netlify.app/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+              <div className='pt-8 text-center'>
+                <a href='https://forkify-v2.netlify.app/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a>
-                <a href="https://github.com/ajfm88/CompleteJavaScriptCourseJonas/tree/main/15-Mapty/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                <a href='https://github.com/ajfm88/CompleteJavaScriptCourseJonas/tree/main/18-forkify/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
                 </a>
@@ -117,21 +116,21 @@ const Work = () => {
           {/* Grid Item */}
           <div
             style={{ backgroundImage: `url(${IMDBClone})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
                 IMDB Clone
               </span>
-              <div className="pt-8 text-center">
-                <a href="https://imdb-clone-ajfm88.vercel.app/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+              <div className='pt-8 text-center'>
+                <a href='https://imdb-clone-ajfm88.vercel.app/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a>
-                <a href="https://github.com/ajfm88/imdb-clone/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                <a href='https://github.com/ajfm88/imdb-clone/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
                 </a>
@@ -140,21 +139,21 @@ const Work = () => {
           </div>
           <div
             style={{ backgroundImage: `url(${SupportDesk})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
                 Support Desk App
               </span>
-              <div className="pt-8 text-center">
-                <a href="https://ajfm88supportdesk.herokuapp.com/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+              <div className='pt-8 text-center'>
+                <a href='https://ajfm88supportdesk.herokuapp.com/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a>
-                <a href="https://github.com/ajfm88/support-desk/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                <a href='https://github.com/ajfm88/support-desk/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
                 </a>
