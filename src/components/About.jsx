6177a29d4b5117f173dfe5191de7a1a9450b0299
unknown <ajfoucaultmo@mail.usf.edu>
2022-06-09 17:48:28 -0700

    Updated 'about' section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,28 +1,29 @@
-import React from "react";
+import React from 'react';
 
 const About = () => {
   return (
-    <div name="about" className="w-full h-screen bg-[#0a192f] text-gray-300">
-      <div className="flex flex-col justify-center items-center w-full h-full">
-        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
-          <div className="sm:text-right pb-8 pl-4">
-            <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+    <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+      <div className='flex flex-col justify-center items-center w-full h-full'>
+        <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+          <div className='sm:text-right pb-8 pl-4'>
+            <p className='text-4xl font-bold inline border-b-4 border-pink-600'>
               About
             </p>
           </div>
           <div></div>
         </div>
-        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
-          <div className="sm:text-right text-4xl font-bold">
+        <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+          <div className='sm:text-right text-4xl font-bold'>
             <p>
               Hi. I'm Alejandro, nice to meet you. Please take a look around.
             </p>
           </div>
-          <div className="text-justify">
+          <div className='text-justify'>
             <p>
-              I'm a very ambitious Software Engineer and front-end developer
-              looking for a role in a tech company with the opportunity to work
-              with the latest technologies on challenging and diverse projects.
+              I'm a very ambitious Software Engineer and Front End Developer
+              working full time for a company in Los Angeles. I am currently
+              also learning about Back End Development and seeking to become a
+              Full Stack Software Engineer within the next 3 to 5 years.
             </p>
             <p> </p>
             <p>
